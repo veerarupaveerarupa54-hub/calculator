793768922e181336698f2f23b1d3e6a865829733
Veera Rupa <veerarupaveerarupa54@gmail.com>
2026-03-11 11:22:01 +0530

    Create palindrome_checker.c

---
+++ palindrome_checker.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, original, remainder, reverse = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num = num / 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome number\n");
+    else
+        printf("Not a palindrome number\n");
+
+    return 0;
+}
